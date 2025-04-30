84d962584169b75abeedc99a8b12832645db48f4
ChandaniSahu <rrrsahu2005@gmail.com>
2025-04-30 20:36:27 +0530

    testing server

--- server/router.js
+++ server/router.js
@@ -10,9 +10,9 @@ router.post('/get-transcript', async (req, res) => {
       const transcriptArray = await YoutubeTranscript.fetchTranscript(videoId);
       const transcriptText = transcriptArray.map(item => item.text).join(' ');
       res.json({ transcript: transcriptText });
-    } catch (error) {
-      console.error('Transcript fetch error:', error);
-      res.status(500).json({ error: error});
+    } catch (err) {
+      console.error('Transcript fetch error:', err);
+      res.status(500).json({ error: err});
     }
   });
   
